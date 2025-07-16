3aca94e4261ac4bd2f32359f1a8cc1d25067ba9c
thalesgmartins <thalesdiniz30@gmail.com>
2025-07-16 15:14:58 -0300

    usando as regras para compatibilidade com tela e VIA

--- keymaps/default/rules.mk
+++ keymaps/default/rules.mk
@@ -2,6 +2,13 @@
 # Otimizações de Compilação
 #
 LTO_ENABLE = yes             # Habilita Link Time Optimization.
+DIP_SWITCH_ENABLE = no
+RGBLIGHT_ENABLE = no
+
+#
+# Contagem de Clicks do Teclado
+#
+WPM_ENABLE = no
 
 #
 # Rotary Encoder
@@ -10,17 +17,14 @@ ENCODER_ENABLE = yes         # Habilita suporte para Rotary Encoder
 ENCODER_MAP_ENABLE = yes     # Habilita mapeamento do encoder, se necessário
 
 #
-# Botão do Rotary Encoder
-#
-DIP_SWITCH_ENABLE = yes      # Ativa botões externos a Matriz Principal
-DIP_SWITCH_MAP_ENABLE = no  # Habilita o mapeamneto desses botões
-
-#
 # Display OLED
 #
-OLED_ENABLE = yes            # Habilita suporte para Display OLED
+OLED_ENABLE = yes
+OLED_DRIVER_ENABLE = yes
+OLED_DRIVER = ssd1306
 
 #
 # VIA
 #
-VIA_ENABLE = no
+VIA_ENABLE = yes
+
